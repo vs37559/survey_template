463927f08d4c5654d702f14b052a5d06ef78c9ed
Vaibhav Srivastava <vaibhav@engagedly.com>
2021-03-23 02:38:09 +0530

    Added rails partials

--- app/controllers/survey_controller.rb
+++ app/controllers/survey_controller.rb
@@ -3,4 +3,24 @@ class SurveyController < ApplicationController
 
   def show
   end
+
+  def survey_el
+  end
+
+  def top_nav
+    @text = params[:text]
+  end
+
+  def survey_section
+  end
+
+  def question_selection
+  end
+
+  def question_mcq
+  end
+
+  def question_tf
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  resources :survey
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope :survey, controller: :survey do
+    get :question, action: :show
+    get :survey_el
+    get :top_nav
+    get :survey_section
+    get :question_selection
+    get :question_mcq
+    get :question_tf
+  end
 end
